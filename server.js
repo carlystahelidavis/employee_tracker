d666e8caea67ac3ab1862dc8a70fd61a62aa389b
Carly Staheli <carlystaheli@carlys-mbp.lan>
2023-05-14 15:11:55 -0600

    functions for view all departments,roles and employees

--- server.js
+++ server.js
@@ -6,7 +6,6 @@ const db = mysql.createConnection(
     {
         host: 'localhost',
         user: 'root',
-        password: '0601',
         database: 'employee_db'
     },
     console.log('Connected to Database.')
@@ -27,9 +26,26 @@ function displayMenu() {
             if (data.menuChoice === "view all departments") {
                 viewAllDepartments()
             }
-            console.log(data.menuChoice);
+            if (data.menuChoice === "view all roles") {
+                viewAllRoles()
+            }
+            if (data.menuChoice === "view all employees") {
+                viewAllEmployees()
+            }
+            if (data.menuChoice === "add a department") {
+                console.log("not ready")
+            }
+            if (data.menuChoice === "add a roll") {
+                console.log("not ready")
+            }
+            if (data.menuChoice === "add an employee") {
+                console.log("not ready")
+            }
+            if (data.menuChoice === "update an employee role") {
+                console.log("not ready")
+            }
         })
-}
+};
 function viewAllDepartments() {
     db.query("SELECT * FROM department", function (err, results) {
         if (err) {
@@ -39,7 +55,24 @@ function viewAllDepartments() {
         console.table(results)
     })
 }
-
+function viewAllRoles() {
+    db.query("SELECT * FROM role", function (err, results) {
+        if (err) {
+            console.log(err)
+            return
+        }
+        console.table(results)
+    })
+}
+function viewAllEmployees() {
+    db.query("SELECT * FROM employee", function (err, results) {
+        if (err) {
+            console.log(err)
+            return
+        }
+        console.table(results)
+    })
+}
 displayMenu();
 //init function calls selection function. Thats where the inquirer options begin. It will create the prompt lists. If prompt answer
 // === option run the db.query function. console.table shows the results in table format. would need to install with npm i
