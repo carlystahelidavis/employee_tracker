c547dbf63180232791a446bd1585c8e783ee35d8
Carly Staheli <carlystaheli@carlys-mbp.lan>
2023-08-06 11:18:23 -0600

    added final logic

--- db/schema.sql
+++ db/schema.sql
@@ -19,7 +19,5 @@ CREATE TABLE employee(
   first_name VARCHAR(30),
   last_name VARCHAR(30),
   role_id INT,
-  manager_id INT,
-  FOREIGN KEY(manager_id) REFERENCES employee(id),
   FOREIGN KEY(role_id) REFERENCES role(id)
 );
--- db/seeds.sql
+++ db/seeds.sql
@@ -1,14 +1,14 @@
 INSERT INTO department (id, name)
-VALUES  (001, "Marketing"),
-        (002, "Accounting"),
-        (003, "Sales");
+VALUES  (001, "Advertising"),
+        (002, "Sales"),
+        (003, "Design");
 
 INSERT INTO role (id, title, salary, department_id)
-VALUES  (001, "Assistant", 45000, 001),
-        (002, "BLah", 55, 002),
-        (003, "dddddd", 456, 003);
+VALUES  (001, "Advertising Manager", 55000, 001),
+        (002, "Account Executive", 120000, 002),
+        (003, "Lead Designer", 95000, 003);
 
--- INSERT INTO employee (id, first_name, last_name, role_id, manager_id)
--- VALUES  (001, "hee"),
---         (002, ""),
---         (003, "");
+INSERT INTO employee (id, first_name, last_name, role_id)
+VALUES  (001, "John", "Smith", 001),
+        (002, "Jane", "Doe", 002),
+        (003, "Logan", "Staheli", 003);
